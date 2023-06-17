711a0ffa079785a55e1476230b047c066f59433f
soojin <2425sja>
2023-06-17 16:36:10 +0900

    소셜로그인 완료

--- naverLogin.js
+++ naverLogin.js
@@ -1,3 +1,5 @@
+const fetch = require("node-fetch");
+
 var express = require("express");
 var app = express();
 var client_id = "vzaCBhI3kCgvN9TIf1YN";
@@ -20,7 +22,7 @@ app.get("/naverlogin", function (req, res) {
       "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>"
   );
 });
-app.get("/callback", function (req, res) {
+app.get("/callback", async function (req, res) {
   const code = req.query.code;
   const state = req.query.state;
   const api_url =
@@ -34,26 +36,52 @@ app.get("/callback", function (req, res) {
     code +
     "&state=" +
     state;
-  var request = require("request");
-  var options = {
-    url: api_url,
-    headers: {
-      "X-Naver-Client-Id": client_id,
-      "X-Naver-Client-Secret": client_secret,
-    },
-  };
+  // var request = require("request");
+  // var options = {
+  //   url: api_url,
+  //   headers: {
+  //     "X-Naver-Client-Id": client_id,
+  //     "X-Naver-Client-Secret": client_secret,
+  //   },
+  // };
 
-  request.get(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
+  const tokenRequest = await (
+    await fetch(api_url, {
+      method: "POST",
+      headers: {
+        "X-Naver-Client-Id": client_id,
+        "X-Naver-Client-Secret": client_secret,
+        Accept: "application/json",
+      },
+    })
+  ).json();
 
-      res.end(body);
-      console.log("body", body);
-    } else {
-      res.status(response.statusCode).end();
-      console.log("error = " + response.statusCode);
-    }
-  });
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const apiUrl = "https://openapi.naver.com/v1/nid/me";
+    const userData = await (
+      await fetch(apiUrl, {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      })
+    ).json();
+    // console.log("access_token", access_token);
+
+    console.log("userData", userData);
+  }
+  return res.redirect("/naverlogin");
+  // request.get(options, function (error, response, body) {
+  //   if (!error && response.statusCode == 200) {
+  //     res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
+
+  //     res.end(body);
+  //     console.log("body", body);
+  //   } else {
+  //     res.status(response.statusCode).end();
+  //     console.log("error = " + response.statusCode);
+  //   }
+  // });
 });
 app.listen(3000, function () {
   console.log("http://127.0.0.1:3000/naverlogin app listening on port 3000!");
